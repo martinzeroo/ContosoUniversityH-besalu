7d29140850f2802edab736940e9c9d9f8806798c
martinzeroo <martinhobesalu@gmail.com>
2023-09-01 12:51:11 +0300

    Created some models

---
+++ ContosoUniversityHõbesalu/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoUniversityHõbesalu.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public  DateTime MyProperty { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
