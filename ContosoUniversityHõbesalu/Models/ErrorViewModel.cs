73468ab9c0be205d28c46d3f7c06d7c4b60f774f
opilane <opilane@HP-CZC321706P>
2023-09-01 11:47:03 +0300

    Add project files.

---
+++ ContosoUniversityHõbesalu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContosoUniversityHõbesalu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
