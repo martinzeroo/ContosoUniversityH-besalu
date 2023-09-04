48e5b2c414e149e37a9fbd69f005fd34059375c1
KiurArbeiter <114060548+martinzeroo@users.noreply.github.com>
2023-09-04 15:35:11 +0300

    Fixed some errors

--- ContosoUniversityHõbesalu/Controllers/StudentController.cs
+++ ContosoUniversityHõbesalu/Controllers/StudentController.cs
@@ -1,6 +1,21 @@
-﻿namespace ContosoUniversityHõbesalu.Controllers
+﻿using ContosoUniversityHõbesalu.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversityHõbesalu.Controllers
 {
-    public class StudentController
+    public class StudentController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public StudentController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
     }
 }
--- ContosoUniversityHõbesalu/Models/Student.cs
+++ ContosoUniversityHõbesalu/Models/Student.cs
@@ -5,6 +5,26 @@
         public int Id { get; set; } 
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public  DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
